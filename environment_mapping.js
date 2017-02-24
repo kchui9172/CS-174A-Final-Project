ca129c76e3802dd300067f81e49ad442c56bf4ef
Kristen <kchui9172@gmail.com>
2017-02-23 23:18:55 -0800

    able to environment map cube

--- environment_mapping.js
+++ environment_mapping.js
@@ -1,10 +1,9 @@
 Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object that our class Canvas_Manager can manage.  This one draws the scene's 3D shapes.
   { 'construct': function( context )
       { this.graphics_state  = context.shared_scratchpad.graphics_state;
-        shapes_in_use[ "box"  ] = new Cube();
-        //maybe better to create 6 squares so can map different textures;
-
+        shapes_in_use["face"] = new Square();
       },
+
     'display': function(time)
       {
         var model_transform = mat4(); 
@@ -12,9 +11,20 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
 
         this.graphics_state.lights = [ new Light( vec4( 0 % 7 - 3, 0 % 6 - 3, 0 % 5 - 3, 1 ), Color( 1, 0, 0, 1 ), 100000000 ),
                                          new Light( vec4( 0 % 6 - 3, 0 % 5 - 3, 0 % 7 - 3, 1 ), Color( 0, 1, 0, 1 ), 100000000 ) ];
+
+        var faceTextures = ["negy.jpg","posy.jpg","negx.jpg","posx.jpg","posz.jpg","negz.jpg"];
         
-        var random_material = new Material( Color( 0,0,0,1), 1, 0, 0, 0, "negx.jpg");
-        shapes_in_use.box.draw( this.graphics_state, model_transform, random_material);
+        var random_material = new Material( Color( 0,0,0,1), 1, 0, 0, 0, faceTextures[0]);
+
+
+        for (var i = 0; i < 3; i++){
+          for (var j = 0; j < 2; j++){
+            var faceImage = new Material( Color( 0,0,0,1), 1, 0, 0, 0, faceTextures[(i*2)+j]);              
+            var square_transform = mult( rotation( i == 0 ? 90 : 0, vec3( 1, 0, 0 ) ), rotation( 180 * j - ( i == 1 ? 90 : 0 ), vec3( 0, 1, 0 ) ) );
+            square_transform = mult( square_transform, translation(0, 0, 100) );
+            shapes_in_use.face.draw(this.graphics_state, square_transform, faceImage); 
+          }
+        }
  
       }
   }, Animation );
