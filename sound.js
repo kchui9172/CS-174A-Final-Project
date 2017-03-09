11edebb12fefb9916de50094e230805246a26c06
Mushi Zhou <mushi@wifi-131-179-11-153.host.ucla.edu>
2017-03-08 17:01:18 -0800

    Adding more sound tracks

--- sound.js
+++ sound.js
@@ -1,13 +1,15 @@
 
 function finishedLoading(bufferList) {
-  // Create two sources and play them both together.
   var source1 = context.createBufferSource();
-  //var source2 = context.createBufferSource();
   source1.buffer = bufferList[0];
-  //source2.buffer = bufferList[1];
 
   source1.connect(context.destination);
-  //source2.connect(context.destination);
   source1.start(0);
-  //source2.start(0);
+}
+
+function playAnimalSound(bufferList, i) {
+  var source1 = context.createBufferSource();
+  source1.buffer = bufferList[i];
+  source1.connect(context.destination);
+  source1.start(i);
 }
