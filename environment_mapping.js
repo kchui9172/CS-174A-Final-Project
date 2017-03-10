a695cba2f4613945f63b70d0f769acfcf4b1bb2b
Kristen <kchui9172@gmail.com>
2017-03-10 12:36:03 -0800

    working on camera movement

--- environment_mapping.js
+++ environment_mapping.js
@@ -2,23 +2,26 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
   { 'construct': function( context )
       { this.graphics_state  = context.shared_scratchpad.graphics_state;
         this.shared_scratchpad    = context.shared_scratchpad;
-        shapes_in_use["face"] = new Square();
+
+        //Load animal models
         shapes_in_use.model_fox       = new ModelFox();
         shapes_in_use.model_bear       = new ModelBear();
-        shapes_in_use["door"] = new Cube();
-        this.graphics_state.lights = [ new Light( vec4( 0, 0, 0, 1 ), Color( 0, 0, 0, 1 ), 100000000)];
-
         shapes_in_use.model_eagle       = new ModelEagle();
         shapes_in_use.model_horse       = new ModelHorse();
         shapes_in_use.model_lion       = new ModelLion();
         shapes_in_use.model_parrot       = new ModelParrot();
+
+        //Load faces for environment mapping cube 
+        shapes_in_use["face"] = new Square();
+        //load door object
+        shapes_in_use["door"] = new Cube();
+        this.graphics_state.lights = [ new Light( vec4( 0, 0, 0, 1 ), Color( 0, 0, 0, 1 ), 100000000)];
       },
     'init_keys': function( controls )   // init_keys():  Define any extra keyboard shortcuts here
       {
         controls.add( "ALT+g", this, function() { this.shared_scratchpad.graphics_state.gouraud       ^= 1; } );   // Make the keyboard toggle some
         controls.add( "ALT+n", this, function() { this.shared_scratchpad.graphics_state.color_normals ^= 1; } );   // GPU flags on and off.
         controls.add( "ALT+a", this, function() { this.shared_scratchpad.animate                      ^= 1; } );
-        //console.log("mew: ",this.shared_scratchpad);
       },
     'update_strings': function( user_interface_string_manager )       // Strings that this displayable object (Animation) contributes to the UI:
       {
@@ -27,7 +30,6 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
       },
     'display': function(time)
       {
-
         console.log("meow:", this.shared_scratchpad.cameraPos);
         var model_transform = mat4(); 
         shaders_in_use[ "Default" ].activate();
@@ -50,27 +52,24 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
             else{
               var faceImage = new Material( Color( 0,0,0,1), 1, 0, 0, 0, faceTextures1[(i*2)+j]);                 
             }
-    
-                     
+                         
             var square_transform = mult( rotation( i == 0 ? 90 : 0, vec3( 100, 0, 0 ) ), rotation( 180 * j - ( i == 1 ? 90 : 0 ), vec3( 0, 100, 0 ) ) );
             square_transform = mult( square_transform, translation(0, 0, 100) );
-            //square_transform = mult( square_transform, translation(0, 0, 1) );
             shapes_in_use.face.draw(this.graphics_state, square_transform, faceImage);               
           }
         }
 
-        var doorMaterial = new Material( Color (0,0,0,1), 1,0,0,0, "door.png");
-
         //draw door
+        var doorMaterial = new Material( Color (0,0,0,1), 1,0,0,0, "door.png");
         model_transform = mat4();
         model_transform = mult( mult( model_transform, translation( -45, 0, -90 ) ), scale(10,20,.2));
         shapes_in_use.door.draw(this.graphics_state, model_transform, doorMaterial);
 
+
+        //Draw animals, different depending on world
         var t = this.graphics_state.animation_time/1000, light_orbit = [ Math.cos(t), Math.sin(t) ];
 
-        //Draw animals
-
-        if (this.shared_scratchpad.worldNum == 1){
+        if (this.shared_scratchpad.worldNum == 1){ //want: goat, horse, cow, and retriever
           model_transform = mat4();
           model_transform = mult( mult( model_transform, translation( -10, -12, -40 ) ), scale(1/60, 1/60, 1/60));
           shapes_in_use.model_horse.set_step( t * 4 );
@@ -80,7 +79,8 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
           model_transform = mult( mult( model_transform, translation( -10, 40, -60 ) ), scale(1/10, 1/10, 1/10));
           shapes_in_use.model_parrot.set_step( t * 4 );
           shapes_in_use.model_parrot       .draw( this.graphics_state, model_transform, purplePlastic );
-        }else if (this.shared_scratchpad.worldNum == 2){
+        }
+        else if (this.shared_scratchpad.worldNum == 2){ //want: elk, moose, bear, and fox
           model_transform = mat4();
           model_transform = mult( mult( model_transform, translation( 10, -12, -80 ) ), scale(1/90, 1/90, 1/90));
           shapes_in_use.model_fox.set_step( t * 8 );
@@ -90,7 +90,8 @@ Declare_Any_Class( "Environment_Mapping",  // An example of a displayable object
           model_transform = mult( mult( model_transform, translation( -90, -12, -30 ) ), scale(1/80, 1/80, 1/80));
           shapes_in_use.model_bear.set_step( t * 1 );
           shapes_in_use.model_bear       .draw( this.graphics_state, model_transform, purplePlastic );
-        }else{
+        }
+        else{ //want: eagle, owl, raven
           model_transform = mat4();
           model_transform = mult( mult( model_transform, translation( -10, 40, -80 ) ), scale(1/100, 1/100, 1/100));
           shapes_in_use.model_eagle.set_step( t * 4 );
--- example-displayables.js
+++ example-displayables.js
@@ -52,81 +52,136 @@ Declare_Any_Class( "Example_Camera",     // An example of a displayable object t
   { 'construct': function( context )     // third-person style camera matrix controls to the canvas.
       { // 1st parameter below is our starting camera matrix.  2nd is the projection:  The matrix that determines how depth is treated.  It projects 3D points onto a plane.
         context.shared_scratchpad.graphics_state = new Graphics_State( translation(0, 0, 0), perspective(90, canvas.width/canvas.height, .1, 1000), 0 );
-        //context.shared_scratchpad.graphics_state = new Graphics_State( translation(0, 0, 0), perspective(45, canvas.width/canvas.height, .1, 1000), 0 );
-        //this.define_data_members( { graphics_state: context.shared_scratchpad.graphics_state, thrust: vec3(), origin: vec3( 0, 5, 0 ), looking: false } );
         this.define_data_members( { graphics_state: context.shared_scratchpad.graphics_state, thrust: vec3(), origin: vec3( 0, 0, 0 ), looking: false, scratchPad: context.shared_scratchpad } );
-
-        console.log("camera pos:", this.scratchPad.cameraPos);
 
         // *** Mouse controls: ***
         this.mouse = { "from_center": vec2() };
       },
     'init_keys': function( controls )   // init_keys():  Define any extra keyboard shortcuts here
       {
-        controls.add( "Space", this, function() { this.thrust[1] = -1; } );     controls.add( "Space", this, function() { this.thrust[1] =  0; }, {'type':'keyup'} );
-        controls.add( "z",     this, function() { this.thrust[1] =  1; } );     controls.add( "z",     this, function() { this.thrust[1] =  0; }, {'type':'keyup'} );
-        controls.add( ",",     this, function() { this.graphics_state.camera_transform = mult( rotation( 6, 0, 0,  1 ), this.graphics_state.camera_transform ); } );
-        controls.add( ".",     this, function() { this.graphics_state.camera_transform = mult( rotation( 6, 0, 0, -1 ), this.graphics_state.camera_transform ); } );
-        controls.add( "r",     this, function() { this.graphics_state.camera_transform = mat4(); this.cameraPos = [0,0,0,1]; } );
+        controls.add( "Space", this, 
+          function() 
+          { 
+            if (this.check_bounds(2)){
+              this.thrust[1] = -1;
+            }else{
+              this.thrust[1] = 0;
+            }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[1] >= 70){
+            //   console.log("can't leave cube");
+            //   this.thrust[1] =  0;
+            // }else{
+            //     this.thrust[1] =  -1; 
+            // }
+          } );     
+
+        controls.add( "Space", this, function() { this.thrust[1] =  0; }, {'type':'keyup'} );
+
+        controls.add( "z",     this, 
+          function() 
+          { 
+            if (this.check_bounds(-2)){
+              this.thrust[1] = 1;
+            }else{
+              this.thrust[0] = 0;
+            }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[1] <= 0){
+            //   console.log("can't leave cube");
+            //   this.thrust[1] =  0;
+            // }else{
+            //     this.thrust[1] =  1; 
+            // }
+          } );     
+
+        controls.add( "z",     this, function() { this.thrust[1] =  0; }, {'type':'keyup'} );
+        // controls.add( ",",     this, function() { this.graphics_state.camera_transform = mult( rotation( 6, 0, 0,  1 ), this.graphics_state.camera_transform ); } );
+        // controls.add( ".",     this, function() { this.graphics_state.camera_transform = mult( rotation( 6, 0, 0, -1 ), this.graphics_state.camera_transform ); } );
+        controls.add( "r",     this, function() { this.graphics_state.camera_transform = mat4(); this.cameraPos = [0,0,0,1];} );
 
         controls.add( "w",     this, 
           function() { 
-            var C_inv = inverse(this.graphics_state.camera_transform);
-            var pos = mult_vec(C_inv, vec4(0,0,0,1));
-            //console.log("posw:",pos);
-            this.scratchPad.cameraPos  = pos;
-            if (pos[2] <= -90){
-              console.log("can't leave cube");
-              this.thrust[2] =  0;
+            if (this.check_bounds(3)){
+              this.thrust[2] = 1;
             }else{
-                this.thrust[2] =  1; 
+              this.thrust[2] = 0;
             }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // //console.log("posw:",pos);
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[2] <= -90){
+            //   console.log("can't leave cube");
+            //   this.thrust[2] =  0;
+            // }else{
+            //     this.thrust[2] =  1; 
+            // }
           } );     
         controls.add( "w",     this, function() { this.thrust[2] =  0; }, {'type':'keyup'} );
 
         controls.add( "a",     this, 
           function() { 
-            var C_inv = inverse(this.graphics_state.camera_transform);
-            var pos = mult_vec(C_inv, vec4(0,0,0,1));
-            //console.log("posa:",pos);
-            this.scratchPad.cameraPos  = pos;
-            if (pos[0] <= -80){
-              console.log("can't leave cube");
-              this.thrust[0] =  0;
+            if (this.check_bounds(-1)){
+              this.thrust[0] = 1;
             }else{
-                this.thrust[0] =  1; 
+              this.thrust[0] = 0;
             }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // //console.log("posa:",pos);
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[0] <= -80){
+            //   console.log("can't leave cube");
+            //   this.thrust[0] =  0;
+            // }else{
+            //     this.thrust[0] =  1; 
+            // }
           } );     
 
         controls.add( "a",     this, function() { this.thrust[0] =  0; }, {'type':'keyup'} );
 
         controls.add( "s",     this, 
           function() { 
-            var C_inv = inverse(this.graphics_state.camera_transform);
-            var pos = mult_vec(C_inv, vec4(0,0,0,1));
-            //console.log("poss:",pos);
-            this.scratchPad.cameraPos  = pos;
-            if (pos[2] >= 90){
-              console.log("can't leave cube");
-              this.thrust[2] = 0;
+            if (this.check_bounds(-3)){
+              this.thrust[2] = -1;
             }else{
-                this.thrust[2] = -1;  
+              this.thrust[2] = 0;
             }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // //console.log("poss:",pos);
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[2] >= 90){
+            //   console.log("can't leave cube");
+            //   this.thrust[2] = 0;
+            // }else{
+            //     this.thrust[2] = -1;  
+            // }
           } );    
         controls.add( "s",     this, function() { this.thrust[2] =  0; }, {'type':'keyup'} );
 
         controls.add( "d",     this, 
           function() {
-            var C_inv = inverse(this.graphics_state.camera_transform);
-            var pos = mult_vec(C_inv, vec4(0,0,0,1));
-            //console.log("posd:",pos);
-            this.scratchPad.cameraPos  = pos;
-            if (pos[0] >= 80){
-              console.log("can't leave cube");
-              this.thrust[0] =  0;
+            if (this.check_bounds(1)){
+              this.thrust[0] = -1;
             }else{
-              this.thrust[0] = -1; 
-            } 
+              this.thrust[0] = 0;
+            }
+            // var C_inv = inverse(this.graphics_state.camera_transform);
+            // var pos = mult_vec(C_inv, vec4(0,0,0,1));
+            // //console.log("posd:",pos);
+            // this.scratchPad.cameraPos  = pos;
+            // if (pos[0] >= 80){
+            //   console.log("can't leave cube");
+            //   this.thrust[0] =  0;
+            // }else{
+            //   this.thrust[0] = -1; 
+            // } 
           } );     
 
         controls.add( "d",     this, function() { this.thrust[0] =  0; }, {'type':'keyup'} );
@@ -139,6 +194,67 @@ Declare_Any_Class( "Example_Camera",     // An example of a displayable object t
         controls.add( "down",     this, function() { console.log("down"); this.graphics_state.camera_transform = mult( rotation( 1, 1, 0, 0 ), this.graphics_state.camera_transform ); } );
 
       },
+      //left = -1, right = 1, up = 2, down = -2, in = 3, out= -3
+
+    'check_bounds' : function(dir){
+      console.log("checking bounds");
+      var C_inv = inverse(this.graphics_state.camera_transform);
+      var pos = mult_vec(C_inv, vec4(0,0,0,1));
+      this.scratchPad.cameraPos = pos;
+
+      if (dir == 1){
+        pos[0] += 1;
+      }
+      if (dir == -1){
+        pos[0] -= 1;
+      }
+      if (dir == 2){
+        pos[1] += 1;
+      }
+      if (dir == -2){
+        pos[1] -= 1;
+      }
+      if (dir == 3){
+        pos[2] -= 1;
+      }
+      if (dir == -3){
+        pos[2] += 1;
+      }
+      // //boundary conditions
+      console.log(pos);
+      // if (pos[0] > 80 && dir != 1){
+      //   //console.log("can't leave cube");
+      //   return true;
+      // }
+      // if (pos[0] < -80 && dir != -1){
+      //   //console.log("can't leave cube");
+      //   return true;
+      // }
+      // if (pos[1] < 0 && dir != -2){
+      //   //console.log("can't leave cube");
+      //   return true;
+      // }
+      // if (pos[1] > 70 && dir != 2){
+      //   // console.log("can't leave cube");
+      //   return true;
+      // }
+      // if (pos[2] < -90 && dir != 3){
+      //   //console.log("can't leave cube");
+      //   return true;
+      // }
+      // if (pos[2] > 90 && dir != -3){
+      //   //console.log("can't leave cube");
+      //   return true;
+      // }
+
+      if (pos[0] > 80 || pos[0] < -80 || pos[1] < 0 || pos[1] > 70 || pos[2] < -90 || pos[2] > 90){
+        console.log("can't leave cube");
+        return false;
+      }
+      else{
+        return true;
+      }
+    },
     'update_strings': function( user_interface_string_manager )       // Strings that this displayable object (Animation) contributes to the UI:
       { var C_inv = inverse( this.graphics_state.camera_transform ), pos = mult_vec( C_inv, vec4( 0, 0, 0, 1 ) ),
                                                                   z_axis = mult_vec( C_inv, vec4( 0, 0, 1, 0 ) );                                                                 
@@ -150,7 +266,7 @@ Declare_Any_Class( "Example_Camera",     // An example of a displayable object t
       { 
 
         //console.log("z", this.scratchPad.cameraPos[2]);
-        if (this.scratchPad.cameraPos[2] <= -80 && this.scratchPad.cameraPos[0] <= -40 && this.scratchPad.cameraPos[0] >= -50){
+        if (this.scratchPad.cameraPos[2] <= -80 && this.scratchPad.cameraPos[0] <= -40 && this.scratchPad.cameraPos[0] >= -50 && this.scratchPad.cameraPos[1] <= 7 && this.scratchPad.cameraPos[1] >= 0){
           console.log("changing worlds");
           //this.scratchPad.worldNum = 2;
           this.scratchPad.worldNum = (this.scratchPad.worldNum % 3) + 1;
@@ -160,19 +276,19 @@ Declare_Any_Class( "Example_Camera",     // An example of a displayable object t
         }
         var leeway = 70,  degrees_per_frame = .0004 * this.graphics_state.animation_delta_time,
                           meters_per_frame  =   .01 * this.graphics_state.animation_delta_time;
-        // Third-person camera mode: Is a mouse drag occurring?
-        if( this.mouse.anchor )
-        {
-          var dragging_vector = subtract( this.mouse.from_center, this.mouse.anchor );            // Arcball camera: Spin the scene around the world origin on a user-determined axis.
-          if( length( dragging_vector ) > 0 )
-            this.graphics_state.camera_transform = mult( this.graphics_state.camera_transform,    // Post-multiply so we rotate the scene instead of the camera.
-                mult( translation( this.origin ),
-                mult( rotation( .05 * length( dragging_vector ), dragging_vector[1], dragging_vector[0], 0 ),
-                      translation(scale_vec( -1, this.origin ) ) ) ) );
-        }
-        // First-person flyaround mode:  Determine camera rotation movement when the mouse is past a minimum distance (leeway) from the canvas's center.
-        var offset_plus  = [ this.mouse.from_center[0] + leeway, this.mouse.from_center[1] + leeway ];
-        var offset_minus = [ this.mouse.from_center[0] - leeway, this.mouse.from_center[1] - leeway ];
+        // // Third-person camera mode: Is a mouse drag occurring?
+        // if( this.mouse.anchor )
+        // {
+        //   var dragging_vector = subtract( this.mouse.from_center, this.mouse.anchor );            // Arcball camera: Spin the scene around the world origin on a user-determined axis.
+        //   if( length( dragging_vector ) > 0 )
+        //     this.graphics_state.camera_transform = mult( this.graphics_state.camera_transform,    // Post-multiply so we rotate the scene instead of the camera.
+        //         mult( translation( this.origin ),
+        //         mult( rotation( .05 * length( dragging_vector ), dragging_vector[1], dragging_vector[0], 0 ),
+        //               translation(scale_vec( -1, this.origin ) ) ) ) );
+        // }
+        // // First-person flyaround mode:  Determine camera rotation movement when the mouse is past a minimum distance (leeway) from the canvas's center.
+        // var offset_plus  = [ this.mouse.from_center[0] + leeway, this.mouse.from_center[1] + leeway ];
+        // var offset_minus = [ this.mouse.from_center[0] - leeway, this.mouse.from_center[1] - leeway ];
 
         for( var i = 0; this.looking && i < 2; i++ )      // Steer according to "mouse_from_center" vector, but don't start increasing until outside a leeway window from the center.
         {
